e6391acfb554d3e2c77d99986eb61f0fcd4f587a
daffa i <daffa2.ismail178@gmail.com>
2026-03-01 12:59:38 +0700

    removed suppressHydrationWarning={true} from layout

--- app/layout.js
+++ app/layout.js
@@ -26,7 +26,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en" >
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased ${sora.variable} font-sora`}
       >
